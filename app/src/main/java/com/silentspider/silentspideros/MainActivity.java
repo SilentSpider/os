bba6f055d9373caf34a3f438d46896ec7f04ad2f
Johan Lundin <johan@bluenine.se>
2016-04-03 14:32:30 +0100

    Removing logic for navigation bar which is hidden by build.prop instead

--- app/src/main/java/com/silentspider/silentspideros/MainActivity.java
+++ app/src/main/java/com/silentspider/silentspideros/MainActivity.java
@@ -55,41 +55,4 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        setFullScreen(true);
-    }
-
-    private void setFullScreen(final boolean fullscreen) {
-        // Hide/show the system bar
-        Window window = getWindow();
-        window.setFlags(fullscreen ?
-                        WindowManager.LayoutParams.FLAG_FULLSCREEN : 0,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-        if (fullscreen) {
-            // Keep the system bar hidden in full screen mode
-            window.getDecorView().setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
-                @Override
-                public void onSystemUiVisibilityChange(int visibility) {
-                    if ((visibility & View.SYSTEM_UI_FLAG_HIDE_NAVIGATION) != 0) {
-                        getWindow().setFlags(
-                                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-                    }
-                    else {
-                        getWindow().getDecorView().setSystemUiVisibility(
-                                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | 0x4);
-                    }
-                }
-            });
-        }
-        else {
-            window.getDecorView().setOnSystemUiVisibilityChangeListener(null);
-        }
-        window.getDecorView().setSystemUiVisibility(
-                fullscreen ? (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | 0x4) : 0);
-    }
 }
